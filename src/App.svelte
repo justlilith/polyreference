b10887a9bc360dbd76ed1cf7dbc3847267946808
justlilith <hello@justlilith.com>
2021-08-04 06:13:58 -0500

    Add moveable library -L6

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Moveable from 'svelte-moveable'
 	import Frame from './components/Frame.svelte'
 	export let name: string;
 	
@@ -8,11 +9,11 @@
 		let coords = { x:0, y:0}
 		let offset = []
 		let currentFrame
-
-	let init = buildFrame('https://i.pinimg.com/originals/10/d1/d3/10d1d39769c54e69a11c409038dc1adc.jpg')
 		
-	frameList.push(init)
-
+		let init = buildFrame('https://i.pinimg.com/originals/10/d1/d3/10d1d39769c54e69a11c409038dc1adc.jpg')
+		
+		frameList.push(init)
+		
 		const handleDragStart = (event, frameid) => {
 			console.log(frameid)
 			event.dataTransfer.setData('frame id', frameid)
@@ -23,14 +24,14 @@
 			return offset // shouldn't change on drag
 		}
 		
-		const drag = (event) => {
+		const dragover = (event) => {
 			coords.x = event.clientX
 			coords.y = event.clientY
 			let id = event.dataTransfer.getData('frame id')
 			let corner = [frameList[id].x, frameList[id].y] //top left
-			frameList[id].x = coords.x - offset[0]
-			frameList[id].y = coords.y - offset[1]
-			frameList[id].style = `position:fixed; left:${frameList[id].x}px; top:${frameList[id].y}px; background-image: url('${frameList[id].url}')`
+			// frameList[id].x = coords.x - offset[0]
+			// frameList[id].y = coords.y - offset[1]
+			frameList[id].style = `position:fixed; left:${frameList[id].x}px; top:${frameList[id].y}px;`
 			console.log(coords, offset, corner)
 			return false
 		}
@@ -59,7 +60,7 @@
 				let newFrame = buildFrame(data)
 				newFrame.x = event.clientX
 				newFrame.y = event.clientY
-				newFrame.style = `position:fixed; left:${newFrame.x}px; top:${newFrame.y}px; background-image: url('${data}')`
+				newFrame.style = `position:fixed; left:${newFrame.x}px; top:${newFrame.y}px;`
 				data ? frameList = [...frameList, newFrame] : null
 				console.log(frameList)
 				console.log(newFrame.style)
@@ -68,7 +69,7 @@
 				let id = event.dataTransfer.getData('frame id')
 				// frameList[id].x = coords.x - frameList[id].x
 				// frameList[id].y = coords.y - frameList[id].y
-				frameList[id].style = `position:fixed; left:${frameList[id].x}px; top:${frameList[id].y}px; background-image: url('${frameList[id].url}')`
+				frameList[id].style = `left:${frameList[id].x}px; top:${frameList[id].y}px; background-image: url('${frameList[id].url}')`
 				console.log(event.dataTransfer.getData('frame id'))
 				console.log(coords)
 			}
@@ -87,7 +88,7 @@
 			let newFrame = buildFrame(data)
 			newFrame.x = 0
 			newFrame.y = 0
-			newFrame.style=`position:fixed; left:${newFrame.x}px; top:${newFrame.y}px; background-image: url('${newFrame.url}')`
+			newFrame.style=`left:${newFrame.x}px; top:${newFrame.y}px;`
 			data ? frameList = [...frameList, newFrame] : null
 			console.log(frameList)
 			console.log(newFrame.style)
@@ -96,8 +97,8 @@
 		function buildFrame (data):FrameT {
 			let frame =
 			{ url: data
-				, width: 100
-				,	height: 100
+				, width: 500
+				,	height: 500
 				, x: 0
 				, y: 0
 				, style: ``
@@ -109,23 +110,58 @@
 			console.log(frame)
 			return frame
 		}
+		
+		let target:Array<HTMLDivElement>=[]
+		let frameOptions = {
+			translate:[0,0]
+		}
 	</script>
 	
 	<main>
 		<!-- <h1>welcome to polyreference, {name}!</h1> -->
 		<!-- <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p> -->
 		<div id='dropzone'
-		on:dragover|preventDefault={event => drag(event)}
 		on:drop|preventDefault={event => drop(event, coords)}
 		on:paste={event => paste(event)}
 		>
 		<!-- on:mousemove={event => trackMouse(event)} -->
 		{#if frameList.length !== 0}
 		{#each frameList as frame}
-		<div on:dragstart={event => offset = handleDragStart(event, frame.id)}>
-			<Frame frame={frame}></Frame>
-
+		<div class='target' bind:this={target[frame.id]}>
+			<!-- <div> -->
+				<Frame frame={frame}></Frame>
 		</div>
+		<Moveable
+		target={target[frame.id]}
+    resizable={true}
+		draggable={true}
+    keepRatio={true}
+    throttleResize={0}
+    renderDirections={["nw","n","ne","w","e","sw","s","se"]}
+    edge={false}
+    zoom={1}
+    origin={false}
+    padding={{"left":0,"top":0,"right":0,"bottom":0}}
+		on:dragStart={({ detail: e })=> {
+			e.set(frameOptions.translate)
+		}}
+		on:drag={({ detail: e })=> {
+			frameOptions.translate = e.beforeTranslate;
+			e.target.style.transform = `translate(${e.beforeTranslate[0]}px, ${e.beforeTranslate[1]}px)`;
+		}}
+		on:resizeStart={({ detail: e }) => {
+			e.setOrigin(["%", "%"]);
+			e.dragStart && e.dragStart.set(frameOptions.translate);
+		}}
+		on:resize={({ detail: e }) => {
+			const beforeTranslate = e.drag.beforeTranslate;
+			
+			frameOptions.translate = beforeTranslate;
+			e.target.style.width = `${e.width}px`;
+			e.target.style.height = `${e.height}px`;
+			e.target.style.transform = `translate(${beforeTranslate[0]}px, ${beforeTranslate[1]}px)`;
+		}}
+		/>
 		{/each}
 		{/if}
 	</div>
@@ -137,6 +173,11 @@
 		/* padding: 1em; */
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	.target {
+		width: 500px;
+    height: 500px;
 	}
 	
 	#dropzone {
--- src/components/Frame.svelte
+++ src/components/Frame.svelte
@@ -2,37 +2,68 @@
   import Fab from '@smui/fab'
   export let frame:FrameT
   
-  let styleConstant:string=` background-image: url('${frame.url}'`
+  let coords = { x:0, y:0}
+  let offset = []
+  
+  let styleConstant:string = ` background-image: url('${frame.url}');`
+  let dimensionalConstant:string = ` width: ${frame.width}px; height: ${frame.height}px;`
+  // frame.style = frame.style + styleConstant + dimensionalConstant
   frame.style = frame.style + styleConstant
-  const handleDrag = (event) => {
-    // console.log(event)
-    // frame.style = `position:fixed; left:${frame.x}px; top:${frame.y}px`
+  
+  const handleDragMove = (event) => {
+    let id = event.dataTransfer.getData('frame id')
+    // frameList[id].x = coords.x - frameList[id].x
+    // frameList[id].y = coords.y - frameList[id].y
+    frame.style = `position: fixed; left: ${frame.x}px; top: ${frame.y}px;`
+    console.log(event.dataTransfer.getData('frame id'))
+    console.log(coords)
   }
   
-  const handleDragStart = (event) => {
+  const handleDragStartMove = (event) => {
     event.dataTransfer.setData('frame id', frame.id)
     event.dataTransfer.dropEffect = 'move'
+    coords.x = event.clientX
+    coords.y = event.clientY
+    offset = [coords.x - frame.x, coords.y - frame.y]
+    return offset
   }
   
+  const handleDragStartResize = (event, edge) => {
+    event.dataTransfer.setData('frame id', frame.id)
+    event.dataTransfer.dropEffect = 'move'
+    coords.x = event.clientX
+    coords.y = event.clientY
+    offset = [coords.x - frame.x, coords.y - frame.y]
+    return offset // shouldn't change on drag
+  }
+  
+  const handleDragResize = (event, edge) => {
+    coords.x = event.clientX
+    coords.y = event.clientY
+    let id = event.dataTransfer.getData('frame id')
+    let corner = {x: frame.x, y: frame.y} //top left
+    console.log(edge)
+    console.log(coords)
+    switch (edge){
+      case 'bright':
+      frame.width = coords.x - corner.x
+      frame.height = coords.y - corner.y
+      let dimensionalConstant:string = ` width: ${frame.width}px; height: ${frame.height}px;`
+      frame.style = styleConstant + dimensionalConstant
+      console.log(frame.style)
+      console.log(frame.width, frame.height)
+      default:
+      return
+    }
+  }
   /*
-https://i.pinimg.com/originals/10/d1/d3/10d1d39769c54e69a11c409038dc1adc.jpg
-*/
+  https://i.pinimg.com/originals/10/d1/d3/10d1d39769c54e69a11c409038dc1adc.jpg
+  */
 </script>
 
 <div
 style={frame.style}
-class='frame'
-draggable='true'
-on:drag={event => handleDrag(event)}
-on:dragstart={event => handleDragStart(event)}>
-<div class='resize-button-frame'>
-<div class='handle-left'></div>
-<div class='handle-right'></div>
-<!-- <div class='resize-button'> -->
-  <!-- <Fab>Resize</Fab> -->
-<!-- </div> -->
-<!-- <img alt='reference' src={frame.url} > -->
-</div>
+class='frame'>
 </div>
 
 <style lang='scss'>
@@ -40,27 +71,27 @@ on:dragstart={event => handleDragStart(event)}>
     width:100%;
     height:100%;
   }
-
+  
   div {
-    height: 200px;
-    width: 200px;
+    @include wh100;
     // display:block;
     background-size: contain;
+    background-repeat: no-repeat;
   }
-
+  
   .resize-button-frame {
     display:grid;
     grid-template-columns: 5% 90% 5%;
     grid-template-rows: 5% 90% 5%;
     grid-template-areas:
     'tleft top tright'
-    'left . right'
+    'left center right'
     'bleft bottom bright';
     // width:100%;
     // height:100%;
     border: thin solid cyan
   }
-
+  
   .handle-left {
     grid-area: left;
     background-color: deepskyblue;
@@ -70,6 +101,11 @@ on:dragstart={event => handleDragStart(event)}>
   .handle-right {
     grid-area: right;
     background-color: deeppink;
+    @include wh100
+  }
+  .handle-bright {
+    grid-area: bright;
+    background-color: silver;
     @include wh100
   }
   .resize-button {
